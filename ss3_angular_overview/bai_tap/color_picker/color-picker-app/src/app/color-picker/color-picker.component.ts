837e0f1e2ecd1ddc5aa46f1c01e35b2eff005aa7
VoCongKhoa <vocongkhoa2208@gmail.com>
2022-05-06 00:21:56 +0700

    upbt

--- ss3_angular_overview/bai_tap/color_picker/color-picker-app/src/app/color-picker/color-picker.component.ts
+++ ss3_angular_overview/bai_tap/color_picker/color-picker-app/src/app/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-color-picker',
@@ -6,23 +6,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-picker.component.css']
 })
 export class ColorPickerComponent implements OnInit {
-  r = 0;
-  g = 0;
-  b = 0;
+  private deg = 0;
+  private r = 0;
+  private g = 0;
+  private b = 0;
+  private per = 0;
 
-  constructor() { }
+  private colorTest3;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.colorTest3 = {
+      'background': `linear-gradient(${this.deg}deg, rgb(${this.r},${this.b},${this.g}), rgb(${this.g},${this.b},${this.r}) ${this.per}%)`,
+      'width': '100%',
+      'height': '200px'
+    }
+    console.log(this.colorTest3)
   }
 
   getColorNumber1(event) {
-    this.r = event.target.value
-  }
-  getColorNumber2(event) {
-    this.g = event.target.value
-  }
-  getColorNumber3(event) {
-    this.b = event.target.value
+    this.r = event.target.value;
+    this.colorTest3 = {
+      // 'background-color': `rgb(${this.r},${this.b},${this.g})`,
+      'background': `linear-gradient(${this.deg}deg, rgb(${this.r},${this.b},${this.g}), rgb(${this.g},${this.b},${this.r}) ${this.per}%)`,
+      'width': '100%',
+      'height': '200px'
+    }
+    console.log(this.colorTest3);
   }
 
+  getColorNumber2(event) {
+    this.g = event.target.value;
+    this.colorTest3 = {
+      'background': `linear-gradient(${this.deg}deg, rgb(${this.r},${this.b},${this.g}), rgb(${this.g},${this.b},${this.r}) ${this.per}%)`,
+      'width': '100%',
+      'height': '200px'
+    }
+    console.log(this.colorTest3);
+  }
+
+  getColorNumber3(event) {
+    this.b = event.target.value;
+    // console.log(this.colorTest3);
+    this.colorTest3 = {
+      'background': `linear-gradient(${this.deg}deg, rgb(${this.r},${this.b},${this.g}), rgb(${this.g},${this.b},${this.r}) ${this.per}%)`,
+      'width': '100%',
+      'height': '200px'
+    }
+    console.log(this.colorTest3);
+  }
+
+
+  getColorNumber0(event) {
+    this.deg = event.target.value;
+    this.colorTest3 = {
+      'background': `linear-gradient(${this.deg}deg, rgb(${this.r},${this.b},${this.g}), rgb(${this.g},${this.b},${this.r}) ${this.per}%)`,
+      'width': '100%',
+      'height': '200px'
+    }
+    console.log(this.colorTest3);
+  }
+
+  getColorNumber4(event) {
+    this.per = event.target.value;
+    this.colorTest3 = {
+      'background': `linear-gradient(${this.deg}deg, rgb(${this.r},${this.b},${this.g}), rgb(${this.g},${this.b},${this.r}) ${this.per}%)`,
+      'width': '100%',
+      'height': '200px'
+    }
+    console.log(this.colorTest3);
+  }
 }
